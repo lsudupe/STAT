720766c5d0dabde0172020c69c10411a60bf6ea6
lsudupe <sudupelaura@gmail.com>
2022-10-01 16:00:13 +0300

    basic code done

--- project1_LS.R
+++ project1_LS.R
@@ -4,10 +4,15 @@
 #### This script is for the first project code
 
 ###Download the data and check it
-library(leaflet)
 library(sf)
+library(epitools)
+library(SpatialEpi)
 library(spData)
 library(ggplot2)
+library(leaflet)
+library(spdep)
+library(INLA)
+
 data("nz")
 class(nz)
 names(nz)
@@ -16,15 +21,6 @@ head(nz)
 
 head(nz$Land_area)
 rownames(nz) <- nz$Name
-
-if (requireNamespace("sf", quietly = TRUE)) {
-  library(sf)
-  summary(nz)
-  ggplot(nz) + 
-    geom_sf(aes(fill = nz$Land_area) +
-    geom_bar())
-}
-
 summary(nz)
 
 ###Add data
@@ -38,19 +34,7 @@ nz$Sex_ratio
 #female:male
 nz$sex <- ifelse(nz$Sex_ratio == 1, 'same', ifelse(nz$Sex_ratio > 1, 'female', "male"))
 
-###Data preparation
-
-# convert to SpatialPolygonsDataFrame
-map_spdf <- as_Spatial(nz, IDs = TRUE) ##EL BUENO
-class(map_spdf)
-plot(map_spdf)
-
-#https://blog.exploratory.io/making-maps-for-new-zealand-regions-and-territories-in-r-4400333df644
-wgs84 = '+proj=longlat +datum=WGS84'
-nz_region <- spTransform(map_spdf, CRS(wgs84))
-
 ###expected cases
-library(epitools)
 ##area: name of the area
 ##Y: observed number of cases, in my case, median income
 ##E: expected number of cases in my case, expected median income
@@ -60,35 +44,33 @@ library(epitools)
 nz$e <- expected(population = nz$Population, cases = nz$Median_income, n.strata = 2)
 nz$SMR <- nz$Median_income / nz$e
 
-# regions additional info
-devtools::install_github("ellisp/nzelect/pkg2")
-library(nzcensus)
-nz_add_data = REGC2013 %>% 
-  select(Name = REGC2013_N, Median_income = MedianIncome2013, 
-         PropFemale2013, PropMale2013) %>% 
-  mutate(Sex_ratio = PropMale2013 / PropFemale2013) %>% 
-  mutate(Name = gsub(" Region", "", Name)) %>% 
-  select(Name, Median_income, Sex_ratio)
-# data join
-nz = left_join(nz, tab, by = "Name") %>% 
-  left_join(nz_add_data, by = "Name") %>% 
-  select(Name, Island, Land_area, Population, Median_income, Sex_ratio)
+if (requireNamespace("sf", quietly = TRUE)) {
+  library(sf)
+  summary(nz)
+  plot(nz)
+}
 
+###Data preparation
+
+# convert to SpatialPolygonsDataFrame
+map_spdf <- as_Spatial(nz, IDs = TRUE) ##EL BUENO
+class(map_spdf)
+#plot(map_spdf)
+
+#https://blog.exploratory.io/making-maps-for-new-zealand-regions-and-territories-in-r-4400333df644
+wgs84 = '+proj=longlat +datum=WGS84'
+nz_region <- spTransform(map_spdf, CRS(wgs84))
 
 ###Mapping median income
-library(leaflet)
 le <- leaflet(nz_region) %>% addTiles()
 
-pale <- colorNumeric(palette = "YlOrRd", domain = nz_region$Median_income)
+pale <- colorNumeric(palette = "YlOrRd", domain = nz_region$SMR)
 
-le %>% addPolygons(color = "grey", weight = 1, fillColor = ~pale(Median_income), fillOpacity = 0.5) %>%
-  addLegend(pal = pale, values = ~Median_income, opacity = 0.5, title = "Median_income", position = "bottomright")
-
-
+le %>% addPolygons(color = "grey", weight = 1, fillColor = ~pale(SMR), fillOpacity = 0.5) %>%
+  addLegend(pal = pale, values = ~SMR, opacity = 0.5, title = "SMR", position = "bottomright")
+######MAYBE ADD ANOTHER VARIABLE MAP
 
 ###Modelling
-library(spdep)
-library(INLA)
 nb_nz <- poly2nb(nz_region)
 head(nb_nz)
 nb_nz[[1]] #the first county has 2 neighbours
@@ -101,16 +83,24 @@ ge <- inla.read.graph(filename = "nz_region.adj")
 nz_region$re_u <- 1:nrow(nz_region@data)
 nz_region$re_v <- 1:nrow(nz_region@data)
 #formula
-formula <- Sex_ratio ~ Median_income + f(re_u, model = "besag", graph = ge, scale.model = TRUE) + f(re_v, model = "iid")
+###expected cases
+##area: name of the area
+##Y: observed number of cases, in my case, median income
+##E: expected number of cases in my case, expected median income
+##quiero que dependa de 
+##population in each 
+##SMR in each area
+formula <- Median_income ~ Sex_ratio + f(re_u, model = "besag", graph = ge, scale.model = TRUE) + f(re_v, model = "iid")
+
 #https://groups.google.com/g/r-inla-discussion-group/c/EPTiPRE7jAM?pli=1 ERROR
-rese <- inla(formula, family = "xpoisson", data = nz_region@data , E = e,
+rese <- inla(formula, family = "poisson", data = nz_region@data , E = e,
              control.predictor = list(compute = TRUE))
+
 #names(inla.models()$likelihood)
 ###results
 summary(rese)
 
-library(ggplot2)
-marginale <- inla.smarginal(rese$marginals.fixed$Median_income)
+marginale <- inla.smarginal(rese$marginals.fixed$Sex_ratio)
 marginale <- data.frame(marginale)
 ggplot(marginale, aes(x = x, y = y)) + geom_line() + labs(x = expression(beta[1]), y = "Density") +
   geom_vline(xintercept = 0, col = "blue") + theme_bw()
@@ -127,19 +117,19 @@ nz_region$UL <- rese$summary.fitted.values[, "0.975quant"]
 pale <- colorNumeric(palette = "YlOrRd", domain = nz_region$RR)
 
 labels <- sprintf("<strong> %s </strong> <br/> Observed: %s <br/> Expected: %s <br/>
-                  Smokers proportion: %s  <br/>RR: %s (%s, %s)",
-                  nz_region$Name, nz_region$Sex_ratio,  round(nz_region$e, 2),  nz_region$Median_income, #round(nz_region$SMR, 2),
+                  Smokers proportion: %s <br/>SMR: %s <br/>RR: %s (%s, %s)",
+                  nz_region$Name, nz_region$Median_income,  round(nz_region$e, 2),  
+                  nz_region$Sex_ratio, round(nz_region$SMR, 2),
                   round(nz_region$RR, 2), round(nz_region$LL, 2), round(nz_region$UL, 2)) %>%
   lapply(htmltools::HTML)
 
 leaflet(nz_region) %>% addTiles() %>%
   addPolygons(color = "grey", weight = 1, fillColor = ~pale(RR),  fillOpacity = 0.5,
               highlightOptions = highlightOptions(weight = 4),
-              #label = labels,
+              label = labels,
               labelOptions = labelOptions(style = list("font-weight" = "normal", padding = "3px 8px"),
                                           textsize = "15px", direction = "auto")) %>%
-  addLegend(pal = pal, values = ~RR, opacity = 0.5, title = "RR", position = "bottomright")
-
+  addLegend(pal = pale, values = ~RR, opacity = 0.5, title = "RR", position = "bottomright")
 
 
 
